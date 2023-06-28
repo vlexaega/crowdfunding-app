927369fd83feb7097df5e7389ed558a60a7add8b
Alex Vega <alexvega@Alexs-MBP.lan>
2023-06-28 11:05:23 -0400

    adds initial handlebars and routes for projects

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,13 +1,29 @@
 const router = require('express').Router();
-const Project = require('../models/Dish');
+const { Project, User } = require('../models');
 
-// route to get all dishes
 router.get('/', async (req, res) => {
-  const projectData = await Project.findAll().catch((err) => {
-    res.json(err);
-  });
-  const projects = projectData.map((projects) => dish.get({ plain: true }));
-  res.render('all', { projects });
+  try {
+    const displayProjects = await Project.findAll({
+      include: [
+        {
+          model: Project,
+          User,
+          attributes: ['filename', 'description'],
+        },
+      ],
+    });
+
+    const projects = displayProjects.map((project) =>
+      project.get({ plain: true })
+    );
+
+    res.render('homepage', {
+      projects,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
